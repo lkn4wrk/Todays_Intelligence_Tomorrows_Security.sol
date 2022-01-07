b13d41aef89f8697c1c8b4b3b3b7fab0d6381aa7
Rainbow Bag Bob <57073899+lkn4wrk@users.noreply.github.com>
2022-01-07 11:00:30 -0800

    Update Rename Token.sol

--- Rename Token.sol
+++ Rename Token.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 contract Nexo is Todays_Intelligence_Tomorrows_Security {
 TokenName = "Nexo";
 newTokenName = "Todays_Intelligence_Tomorrows_Security";
+TokenSymbol = "NEXO";
+newTokenSymbol = "TIT$";
 }
